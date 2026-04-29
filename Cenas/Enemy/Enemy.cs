4c29800a1c241f9a1bb2feb4333dae8f1c003505
LaraMatosAguirres <lara.laramatos@gmail.com>
2026-04-28 21:00:36 -0300

    Implement enemy in game

---
+++ Cenas/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Enemy : Area2D
+{
+
+	[Export] public Player player;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (player == null)
+			return;
+		Vector2 direction = (player.GlobalPosition - GlobalPosition + Vector2.One * 100).Normalized();
+		Position += direction * 50 * (float)delta;
+	}
+}
